c99d1320d6ad50048a2fd7d5c00d6f87bdbcc5fb
Manuel Plank <manuel.plank@melli.com>
2023-06-30 15:34:39 +0200

    gitignore target dir

--- rust-shell/target/debug/deps/rust_shell-50036a00df65a90d.d
+++
@@ -1,5 +0,0 @@
-/home/manuel/Projects/nix-templates/rust-shell/target/debug/deps/rust_shell-50036a00df65a90d.rmeta: src/main.rs
-
-/home/manuel/Projects/nix-templates/rust-shell/target/debug/deps/rust_shell-50036a00df65a90d.d: src/main.rs
-
-src/main.rs:
