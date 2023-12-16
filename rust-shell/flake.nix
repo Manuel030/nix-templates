0ee2120cd405e9d28e0de09647d23994612f5f3a
Manuel Plank <manuel.plank@melli.com>
2023-12-16 11:26:55 +0100

    remove duplicate inputs in rust overlay

--- rust-shell/flake.nix
+++ rust-shell/flake.nix
@@ -2,20 +2,27 @@
   description = "Rust developtment environment";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ 
-            (import rust-overlay) 
-            (self: super: {
-                rustToolchain = super.rust-bin.selectLatestNightlyWith
-                  (toolchain: toolchain.default.override { extensions = [ "rust-src" "rust-analyzer" ]; }); })
-            ];
+        overlays = [
+          (import rust-overlay)
+          (self: super: {
+            rustToolchain = super.rust-bin.selectLatestNightlyWith
+              (toolchain: toolchain.default.override { extensions = [ "rust-src" "rust-analyzer" ]; });
+          })
+        ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
@@ -23,12 +30,12 @@
       with pkgs;
       {
         devShells.default = mkShell {
-            packages = (with pkgs; [
-                rustToolchain
-                openssl
-                pkg-config
-            ]);
-            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          packages = (with pkgs; [
+            rustToolchain
+            openssl
+            pkg-config
+          ]);
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
       }
     );
